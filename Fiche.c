0575c26cb5c507c6b8e21108829da7a5631c14cb
ThanhTriNath <Nathan.Lieutaud@gmail.com>
2025-04-21 18:47:15 +0200

    Add files via upload

---
+++ Fiche.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int age=12;
+    char prénom []="Nathan";
+    float taille=1.46;
+
+    printf ("Age %d \n", age);
+    printf ("Prénom %s \n", prénom);
+    printf ("Taille %.2f\n", taille);
+    
+    return 0;
+    }
+    
